122c0ad352d6f621c323b972a6601dc15b0f4918
aska <enkhbadralasral00@gmail.com>
2025-03-04 17:30:50 +0000

    forgot to takeout my env

--- frontend/src/store/useAuthStore.jsx
+++ frontend/src/store/useAuthStore.jsx
@@ -33,5 +33,40 @@ export const useAuthStore = create((set) => ({
             set({isSigningUp: false});
         }
     },
+    logout: async()=>{
+
+        try{
+           await axiosInstance.post("/auth/logout");
+            set({authUser: null});
+            toast.success("Succesfully loggout");
+        }catch(error){
+            toast.error(error.response.data.message);
+        }
+    },
+    login: async(data)=>{
+        set({isLoggingIn:true});
+        try{
+            const res = await axiosInstance.post("/auth/login", data);
+            set({authUser: res.data});
+            toast.success("Logged in");
+        }catch(error){
+            toast.error(error.response.data.message);
+        }finally{
+            set({isLoggingIn:false});
+        }
+    },
+    updateProfile: async (data)=>{
+        set({ isUpdatingProfile: true });
+        try {
+          const res = await axiosInstance.put("/auth/update-profile", data);
+          set({ authUser: res.data });
+          toast.success("Profile updated successfully");
+        } catch (error) {
+          console.log("error in update profile:", error);
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isUpdatingProfile: false });
+        }
+    }
 }));
 
