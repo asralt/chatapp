c743683e23848b9f2d6e9b98fbf24b58ffe7b9fb
aska <enkhbadralasral00@gmail.com>
2025-03-06 12:03:55 +0000

    chnaged index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173","https://chatapp-vcu9.onrender.com",
+    origin: ["http://localhost:5173","https://chatapp-vcu9.onrender.com"],
     credentials: true,
   })
 );
