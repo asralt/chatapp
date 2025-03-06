03e34ec4fc229857cf2474bac07a02a22a0d3807
aska <enkhbadralasral00@gmail.com>
2025-03-06 12:03:45 +0000

    changed index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,14 +14,14 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173","https://chatapp-vcu9.onrender.com",
     credentials: true,
   })
 );
@@ -29,13 +29,13 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// ✅ Serve frontend always (not just in production)
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
